37ef0312574a797c7cab803b8d79d3d91ac6f40f
Ramiro Charry <ramiro22_cba@hotmail.com>
2022-11-13 21:06:09 -0300

    prueba

---
+++ js/funciones.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(listo);
+
+function listo()
+{
+    jQuery(".hamb").click(function(e){
+        e.preventDefault();
+        jQuery("header .container nav").toggleClass("open");
+        jQuery(".hamb i").toggleClass("fa-times");
+    
+    });   
+
+    jQuery("header .container nav a").click(function(){
+
+        jQuery("header .container nav").removeClass("open");
+        jQuery(".hamb i").removeClass("fa-times");
+
+        var link = jQuery(this).attr("href");
+
+        jQuery("html,body").animate({
+            "scrollTop": jQuery(link).offset().top -70
+        
+        })
+        jQuery(".bloque-inicio a.boton.boton-rojo").animate({
+            "scrollTop": jQuery(link).offset().top
+        })
+        jQuery()
+    })
+    
+}
+
